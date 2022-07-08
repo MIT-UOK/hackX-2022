bb865a9e1b94bdf223b1e4f4a0500325476511ca
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-07-09 01:26:55 +0530

    initial commit

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss')
+module.exports = {
+    plugins: [
+        'postcss-preset-env',
+        tailwindcss,
+        purgecss({
+            content: ['./**/*.html']
+        })
+    ],
+};
