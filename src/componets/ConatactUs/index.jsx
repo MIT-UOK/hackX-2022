f36292d09e7303b807172d865eb819cdb6309a8b
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-07-20 16:19:13 +0530

    added contact section content

--- src/componets/ConatactUs/index.jsx
+++ src/componets/ConatactUs/index.jsx
@@ -1,13 +1,33 @@
 import HeadTile from "../HeadTitle";
+import ConnectCard from "./ConnectCard";
+import Form from "./Form";
 
 function ContactUs() {
     return (
         <div className="bg-homeBg1 pt-20">
             <HeadTile title={"CONTACT"} />
-            <div className="container mx-auto">
+            <div className="container px-3 md:px-0 mx-auto">
+                <div className="mt-12 grid grid-cols-1 gap-10 sm:mt-16 md:grid-cols-2  ">
+                    <div className="">
+                        <div className="max-w-[570px]">
+                            <div className="p-5">
 
+                                <ConnectCard />
+                                <ConnectCard />
+                                <ConnectCard />
+                            </div>
+
+                        </div>
+                    </div>
+                    <div>
+                        <div className="bg-white p-5 rounded-xl">
+                            <Form />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+
     );
 }
 
