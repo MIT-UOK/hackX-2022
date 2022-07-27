ea2e9ef5db8402d6ad7b956cea1c6302180714c5
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-07-27 15:49:19 +0530

    fix: === error

--- src/componets/TimeLine/index.jsx
+++ src/componets/TimeLine/index.jsx
@@ -2,7 +2,6 @@ import HeadTile from '../HeadTitle';
 import "./index.css"
 import { useState } from "react";
 import Paragraph from "../Paragraph"
-import { BsFillCalendarCheckFill } from "react-icons/bs"
 import IconText from './IconText';
 
 
@@ -22,11 +21,11 @@ function TimeLine() {
 
             <div id="progress-bar-container">
                 <ul>
-                    <li className={selectedIndex == 0 ? "step active" : "step"} onClick={() => callLineWidth(0)}><div className="step-inner">Registrations</div></li>
-                    <li className={selectedIndex == 1 ? "step active" : "step"} onClick={() => callLineWidth(1)}><div className="step-inner">Proposal submission</div></li>
-                    <li className={selectedIndex == 2 ? "step active" : "step"} onClick={() => callLineWidth(2)}><div className="step-inner">hackX Initial Ideation</div></li>
-                    <li className={selectedIndex == 3 ? "step active" : "step"} onClick={() => callLineWidth(3)}><div className="step-inner">designX</div></li>
-                    <li className={selectedIndex == 4 ? "step active" : "step"} onClick={() => callLineWidth(4)}><div className="step-inner">hackX 2022: Finals</div></li>
+                    <li className={selectedIndex === 0 ? "step active" : "step"} onClick={() => callLineWidth(0)}><div className="step-inner">Registrations</div></li>
+                    <li className={selectedIndex === 1 ? "step active" : "step"} onClick={() => callLineWidth(1)}><div className="step-inner">Proposal submission</div></li>
+                    <li className={selectedIndex === 2 ? "step active" : "step"} onClick={() => callLineWidth(2)}><div className="step-inner">hackX Initial Ideation</div></li>
+                    <li className={selectedIndex === 3 ? "step active" : "step"} onClick={() => callLineWidth(3)}><div className="step-inner">designX</div></li>
+                    <li className={selectedIndex === 4 ? "step active" : "step"} onClick={() => callLineWidth(4)}><div className="step-inner">hackX 2022: Finals</div></li>
                 </ul>
 
                 <div id="line">
@@ -35,33 +34,33 @@ function TimeLine() {
             </div>
 
             <div id="progress-content-section">
-                <div className={selectedIndex == 0 ? "section-content active" : "section-content"}>
+                <div className={selectedIndex === 0 ? "section-content active" : "section-content"}>
                     <div className='text-center text-white uppercase text-lg font-bold pb-5'>Registrations</div>
                     <IconText type="time" details="July 13th - August 3rd , 2021" />
                     <Paragraph color="text-white" text={"Once the registrations are opened, fill out the team details through the registration form and download the sample proposal structure via www.hackX.lk"} />
                 </div>
 
-                <div className={selectedIndex == 1 ? "section-content active" : "section-content"}>
+                <div className={selectedIndex === 1 ? "section-content active" : "section-content"}>
                     <div className='text-center text-white uppercase text-lg font-bold pb-5'>Proposal submission</div>
                     <IconText type="time" details="July 13th - August 3rd , 2021" />
                     <Paragraph color="text-white" text={"The proposals prepared according to the sample structure provided should be submitted via www.hackx.lk."} />
                 </div>
 
-                <div className={selectedIndex == 2 ? "section-content active" : "section-content"}>
+                <div className={selectedIndex === 2 ? "section-content active" : "section-content"}>
                     <div className='text-center text-white uppercase text-lg font-bold pb-5'>hackX Initial Ideation</div>
                     <IconText type="time" details="July 13th - August 3rd , 2021" />
                     <IconText type="location" details="Multimedia Room, Department of Industrial Management, University of Kelaniya" />
                     <Paragraph color="text-white" text={"30 teams with the most innovative ideas will be selected after the initial screening process. Each team will get 5 minutes to pitch their ideas infront of a distinguished panel of judges. The top 12 teams will compete in hackX 2022  finals."} />
                 </div>
 
-                <div className={selectedIndex == 3 ? "section-content active" : "section-content"}>
+                <div className={selectedIndex === 3 ? "section-content active" : "section-content"}>
                     <div className='text-center text-white uppercase text-lg font-bold pb-5'>designX</div>
                     <IconText type="time" details="July 13th - August 3rd , 2021" />
                     <IconText type="location" details="Via Zoom" />
                     <Paragraph color="text-white" text={"4 webinar sessions will be conducted to guide the finalists enhancing their skills."} />
                 </div>
 
-                <div className={selectedIndex == 4 ? "section-content active" : "section-content"}>
+                <div className={selectedIndex === 4 ? "section-content active" : "section-content"}>
                     <div className='text-center text-white uppercase text-lg font-bold pb-5'>hackX 2022: Finals</div>
                     <IconText type="time" details="July 13th - August 3rd , 2021" />
                     <IconText type="location" details="Ideamart Auditorium, Dialog Axiata " />
