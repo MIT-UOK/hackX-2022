38fb8f9f42cf156d88ca3115eea827351f950550
Ishan-Ravindu <ishanravindu27596@gmail.com>
2022-10-15 11:49:11 +0530

    added pathners section

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Footer from "./componets/Footer"
 import Info from "./componets/Info"
 import Guidelines from "./componets/Guidelines"
 import Team from "./componets/Team"
-// import Partners from "./componets/Partners"
+import Partners from "./componets/Partners"
 
 function App() {
 
@@ -22,7 +22,7 @@ function App() {
       <TimeLine />
       <Awards />
       <Team />
-      {/* <Partners /> */}
+      <Partners />
       <ContactUs />
       <Footer />
     </>
--- src/componets/Navbar/MenuItems.js
+++ src/componets/Navbar/MenuItems.js
@@ -24,11 +24,11 @@ export const MenuItems = [
     url: "#OUR-TEAM",
     cName: "navlinks"
   },
-  // {
-  //   title: "PARTNERS",
-  //   url: "#PARTNERS",
-  //   cName: "navlinks"
-  // },
+  {
+    title: "PARTNERS",
+    url: "#PARTNERS",
+    cName: "navlinks"
+  },
   {
     title: "CONTACT",
     url: "#CONTACT",
